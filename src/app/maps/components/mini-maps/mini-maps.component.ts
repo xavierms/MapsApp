f043eca5c6583fc6baf36ac2e1749ac0b4a4a112
Xavier Mejía <xavierams08@gmail.com>
2022-01-30 18:28:36 -0400

    mini maps on property components

--- src/app/maps/components/mini-maps/mini-maps.component.ts
+++ src/app/maps/components/mini-maps/mini-maps.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-maps',
   templateUrl: './mini-maps.component.html',
-  styles: [
+  styles: [`
+    
+    div{
+      width:100%;
+      height:150%;
+      margin:0%;
+    }
+  `
   ]
 })
-export class MiniMapsComponent implements OnInit {
-
+export class MiniMapsComponent implements AfterViewInit {
+@Input() lngLat: [number,number] =[0,0];
+@ViewChild('map') divMap!: ElementRef
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+      new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map);
   }
 
 }
--- src/app/maps/pages/properties/properties.component.ts
+++ src/app/maps/pages/properties/properties.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface Property {
+  title: string;
+  description: string;
+  lngLat: [number, number];
+}
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
   styles: [
   ]
 })
-export class PropertiesComponent implements OnInit {
+export class PropertiesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  properties: Property[] = [
+    {
+      title: 'Residential house, Canadá',
+      description: 'Beaty property in Katana, Canadá',
+      lngLat: [ -75.92722289474008, 45.280015511264466]
+    },
+    {
+      title: 'Beach house, México',
+      description: 'Beautiful beach house in Acapulco, México',
+      lngLat: [ -99.91287720907991, 16.828940930185748]
+    },
+    {
+      title: 'Apartament, Argentina',
+      description: 'Luxurious apartment in the heart of Buenos Aires, Argentina',
+      lngLat: [ -58.430166677283445, -34.57150108832866 ]
+    },
+    {
+      title: 'Shop store, España',
+      description: 'Shop store premises available in Madrid, España, near El Jardín Secreto.',
+      lngLat: [ -3.7112735618380177, 40.42567285425766 ]
+    },
+  ]
+ 
 
 }
